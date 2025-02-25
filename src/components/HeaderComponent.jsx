38638c9870cc10cbfd8e983eb0f1d7698d65812c
vadimzbanok <vadimzbanok@gmail.com>
2025-02-25 15:17:47 +0100

    style header component

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,11 +3,60 @@ import { NavLink, Link } from "react-router-dom";
 const HeaderComponent = () => {
   return (
     <>
-      <nav>
-        <NavLink to="/" >Home</NavLink>
-        <NavLink to="/about" >About</NavLink>
-        <NavLink to="/meals" >Meals</NavLink>
-      </nav>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">Über Uns</NavLink>
+              </li>
+              <li>
+                <NavLink to="/meals">Meals</NavLink>
+              </li>
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost text-xl">
+            RMeals
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/meals">Meals</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
     </>
   );
 };
